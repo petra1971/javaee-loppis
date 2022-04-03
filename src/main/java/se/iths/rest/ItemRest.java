c2b2db87a29fbaa362d1a0d6a6f9b39543754e12
Petra Andreasson <petra.andreasson@iths.se>
2022-04-03 19:27:07 +0200

    Refactor paths to best practice, add WebApplicationException in getItemById method, add updateName method, add different variants of JPQL queries for demo,  refactor getAllItemsSortedByCategoryCriteria method to return Response object instead of List in ItemRest and ItemService classes

--- src/main/java/se/iths/rest/ItemRest.java
+++ src/main/java/se/iths/rest/ItemRest.java
@@ -1,6 +1,7 @@
 package se.iths.rest;
 
 import se.iths.entity.Item;
+import se.iths.entity.User;
 import se.iths.service.ItemService;
 
 import javax.inject.Inject;
@@ -9,7 +10,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Path("item")
+@Path("items")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class ItemRest {
@@ -17,15 +18,15 @@ public class ItemRest {
     @Inject
     ItemService itemService;
 
-    @Path("new")
+    @Path("")
     @POST
     public Response createItem(Item item) {
         itemService.createItem(item);
         return Response.ok(item).build();
     }
 
-    @Path("update")
-    @PUT
+    @Path("")
+    @PUT //input a complete json-object for the item in body, no id in the url. e.g. url: http://localhost:8080/javaee-loppis/items and in body {	"category": "Möbler", "createdAt": "2022-03-30", "name": "Soffa","price": 500.0,"quantity": 155}
     public Response updateItem(Item item) {
         itemService.updateItem(item);
         return Response.ok(item).build();
@@ -33,12 +34,17 @@ public class ItemRest {
 
     @Path("{id}")
     @GET
-    public Response getItem(@PathParam("id") Long id) {
+    public Response getItemById(@PathParam("id") Long id) {
         Item foundItem = itemService.findItemById(id);
+        if (foundItem == null) {
+            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+                    .entity("Item with ID" + id + " was not found in database.")
+                    .type(MediaType.TEXT_PLAIN_TYPE).build());
+        }
         return Response.ok(foundItem).build();
     }
 
-    @Path("getall")
+    @Path("")
     @GET
     public List<Item> getAllItems() {
         return itemService.getAllItems();
@@ -51,4 +57,86 @@ public class ItemRest {
         return Response.ok().build();
     }
 
+
+    @Path("{id}")
+    @PATCH  //input the field that is to be updated in body e.g. {"name": "Soffa"}, need id in the url. url: http://localhost:8080/javaee-loppis/items/8
+    public Response updateName(@PathParam("id") Long id, Item item) {
+        Item updatedItem = itemService.updateName(id, item.getName());
+        return Response.ok(updatedItem).build();
+    }
+
+    //JPQL queries for demo
+
+    // Dynamiq query
+    @Path("getbycategory-dq")
+    @GET
+    public List<Item> getByCategoryDQ(@QueryParam("category") String category) {
+        return itemService.getByCategoryDynamicQuery(category);
+    }
+
+    // Named parameters
+    @Path("getbycategory-np")
+    @GET
+    public List<Item> getByCategoryNamedParameters(@QueryParam("category") String category) {
+        return itemService.getByCategoryNamedParameters(category);
+    }
+
+    // Named parameters
+    @Path("getbycategory-pp")
+    @GET
+    public List<Item> getByCategoryPositionalParameters(@QueryParam("category") String category) {
+        return itemService.getByCategoryPositionalParameters(category);
+    }
+
+    // Where between
+    @Path("getbetweenprices")
+    @GET
+    public List<Item> getBetweenPrices(@QueryParam("minPrice") double minPrice,
+                                            @QueryParam("maxPrice") double maxPrice) {
+        return itemService.getItemsBetweenPrices(minPrice, maxPrice);
+    }
+
+    // Order by (alphabetically)
+    @Path("getallorderedbycategory")
+    @GET
+    public List<Item> getAllItemsOrderedByCategory() {
+        return itemService.getAllItemsOrderedByCategory();
+    }
+
+    // Named query - get all
+    @Path("getall-nq")
+    @GET
+    public List<Item> getAllItemsNamedQuery() {
+        return itemService.getAllItemsNamedQuery();
+    }
+
+    // Typed query
+    @Path("getmaxprice")
+    @GET
+    public Double getMaxPrice() {
+        return itemService.getHighestPrice();
+    }
+
+    // Criteria query -gett all sorted y category
+    @Path("getallitems-cq")
+    @GET
+    public List<Item> getAllItemsCriteriaQuery() {
+        return itemService.getAllCriteria();
+    }
+
+    @Path("getallitemssortedbycategory-cq")
+    @GET
+//    public List<Item> getAllItemsSortedByCategoryCriteria() {
+//        return itemService.getAllItemsSortedByCategoryCriteria();
+//    }
+    public Response getAllItemsSortedByCategoryCriteria() {
+        List<Item> items = itemService.getAllItems();
+        return Response.ok(items).build();
+
+    }
+
+
+
+
 }
+
--- src/main/java/se/iths/service/ItemService.java
+++ src/main/java/se/iths/service/ItemService.java
@@ -4,6 +4,10 @@ import se.iths.entity.Item;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 import java.util.List;
 
@@ -26,6 +30,7 @@ public class ItemService {
         return entityManager.find(Item.class, id);
     }
 
+    // JPQL Dynamic query
     public List<Item> getAllItems() {
         return entityManager.createQuery("SELECT i from Item i", Item.class).getResultList();
     }
@@ -34,4 +39,77 @@ public class ItemService {
         Item itemToRemove = entityManager.find(Item.class, id);
         entityManager.remove(itemToRemove);
     }
+
+    public Item updateName(Long id, String name) {
+        Item foundItem = entityManager.find(Item.class, id);
+        //Nu finns foundItem i vårt Persistence Context
+        foundItem.setName(name);
+        // Ändringen registreras i vårt Percistence Context
+        return foundItem;
+    }
+
+    //JPQL queries
+
+    // Dynamic query - risk for sql injection
+    public List<Item> getByCategoryDynamicQuery(String category) {
+        return entityManager.createQuery("SELECT i from Item i WHERE i.category = '" + category + "'", Item.class).getResultList();
+    }
+
+    //Named query - mer säker
+    public List<Item> getByCategoryNamedParameters(String category) {
+        String query = "SELECT i from Item i WHERE i.category = :category";
+        return entityManager.createQuery(query, Item.class).setParameter("category", category).getResultList();
+    }
+
+    //Positional parameters
+    public List<Item> getByCategoryPositionalParameters(String category) {
+        String query = "SELECT i from Item i WHERE i.category = ?1";
+        return entityManager.createQuery(query, Item.class).setParameter(1, category).getResultList();
+    }
+
+    //Where between
+    public List<Item> getItemsBetweenPrices(double minPrice, double maxPrice) {
+        String query = "SELECT i from Item i WHERE i.price BETWEEN :minPrice AND :maxPrice";
+        return entityManager.createQuery(query, Item.class).setParameter("minPrice", minPrice)
+                .setParameter("maxPrice", maxPrice).getResultList();
+    }
+
+    // Order by (alphabetically)
+    public List<Item> getAllItemsOrderedByCategory() {
+        String query = "SELECT i from Item i ORDER BY i.category";
+        return entityManager.createQuery(query, Item.class).getResultList();
+    }
+
+    // Named query
+    public List<Item> getAllItemsNamedQuery() {
+        return entityManager.createNamedQuery("itemEntity.findAll", Item.class).getResultList();
+    }
+
+    //Typed query-select the highest price from all items
+    public double getHighestPrice() {
+        TypedQuery<Double> typedQuery = entityManager.createQuery("SELECT MAX(i.price) from Item i", Double.class);
+        return typedQuery.getSingleResult();
+    }
+
+    // Criteria API queries - typsäkert sätt som underlättar refakturering t ex byte av namn på parameter "name" skulle inte uppfattas av compilatorn eftersom name är en
+
+    // Get all items (no rest-method created)
+    public List<Item> getAllCriteria() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Item> criteriaQuery = criteriaBuilder.createQuery(Item.class);
+        TypedQuery<Item> typedQuery = entityManager.createQuery(criteriaQuery);
+        return typedQuery.getResultList();
+    }
+
+    // Order by category (no rest-method created)
+    public List<Item> getAllItemsSortedByCategoryCriteria() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Item> criteriaQuery = criteriaBuilder.createQuery(Item.class);
+        Root<Item> itemRoot = criteriaQuery.from(Item.class);
+        criteriaQuery.orderBy(criteriaBuilder.asc(itemRoot.get("category")));
+        TypedQuery<Item> typedQuery = entityManager.createQuery(criteriaQuery);
+        return typedQuery.getResultList();
+    }
+
+
 }
